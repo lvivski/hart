7d669d6c4199e4f69058e04ee5e440e5fbc74deb
Yehor Lvivski <lvivski@gmail.com>
2013-01-23 22:24:51 +0200

    better runner

--- tool/run_tests.sh
+++ tool/run_tests.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../test" && pwd )"
+set -e
 
-dart --enable-checked-mode $ROOT_DIR/hart_test.dart
+DIR=$( cd $( dirname "${BASH_SOURCE[0]}" )/.. && pwd )
+
+echo "Analyzing library for warnings or type errors"
+dart_analyzer --fatal-warnings --fatal-type-errors lib/*.dart
+rm -r out
+
+for test in $DIR/test/*_test.dart
+do
+	echo -e "\nRunning test suite: $(basename $test)"
+	dart --checked $test
+done
+
+echo -e "\n[32m✓ OK[0m"
